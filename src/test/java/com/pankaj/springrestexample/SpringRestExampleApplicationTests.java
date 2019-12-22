f49c809a10cfadbcf457346f22530eb581dcdcdf
pankajrana737 <pankajrana737@gmail.com>
2019-12-22 07:56:58 +0530

    new file:   .gitignore 	new file:   .mvn/wrapper/MavenWrapperDownloader.java 	new file:   .mvn/wrapper/maven-wrapper.jar 	new file:   .mvn/wrapper/maven-wrapper.properties 	new file:   mvnw 	new file:   mvnw.cmd 	new file:   pom.xml 	new file:   src/main/java/com/pankaj/springrestexample/SpringRestExampleApplication.java 	new file:   src/main/resources/application.properties 	new file:   src/test/java/com/pankaj/springrestexample/SpringRestExampleApplicationTests.java

---
+++ src/test/java/com/pankaj/springrestexample/SpringRestExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pankaj.springrestexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
