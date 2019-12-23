7d1fde506fbc23c9b31cab5721b6a4a82cd90680
pankajrana737 <pankajrana737@gmail.com>
2019-12-23 13:20:06 +0530

    modified:   pom.xml 	new file:   src/main/java/com/pankaj/springrestexample/HelloWordlController.java 	new file:   src/main/java/com/pankaj/springrestexample/model/User.java 	new file:   src/main/java/com/pankaj/springrestexample/service/UserDaoService.java 	modified:   src/main/resources/application.properties

---
+++ src/main/java/com/pankaj/springrestexample/HelloWordlController.java
@@ -0,0 +1,24 @@
+package com.pankaj.springrestexample;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.pankaj.springrestexample.model.User;
+import com.pankaj.springrestexample.service.UserDaoService;
+
+@RestController
+public class HelloWordlController {
+	
+	@Autowired
+	UserDaoService userService;
+	
+	@GetMapping("/users")
+	public List<User> getAllUser(){
+		return userService.reteriveAllUsers();
+		
+	}
+
+}
---
+++ src/main/java/com/pankaj/springrestexample/service/UserDaoService.java
@@ -0,0 +1,35 @@
+package com.pankaj.springrestexample.service;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.stereotype.Component;
+
+import com.pankaj.springrestexample.model.User;
+
+@Component
+public class UserDaoService {
+	
+ private static int userId=6;
+ private static List<User> users= new ArrayList<>();
+ static {
+	 users.add(new User(1, "Pankaj Rana", new Date()));
+
+	 users.add(new User(2, "Tommy", new Date()));
+
+	 users.add(new User(3, "Jeery", new Date()));
+	 
+
+	 users.add(new User(4, "John doe", new Date()));
+
+	 users.add(new User(5, "Merry Jane Rana", new Date()));
+
+
+ }
+ //
+public List<User> reteriveAllUsers(){
+	return users;
+}
+	
+}
