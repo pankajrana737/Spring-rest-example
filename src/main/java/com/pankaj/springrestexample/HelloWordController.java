5074da5394c555f1932eab31e325cbcfc53f4fa7
pankajrana737 <pankajrana737@gmail.com>
2019-12-24 15:24:46 +0530

    modified:   pom.xml 	modified:   src/main/java/com/pankaj/springrestexample/HelloWordController.java 	modified:   src/main/resources/messages.properties 	modified:   src/main/resources/messages_fr.properties 	new file:   src/main/resources/messages_rus.properties 	new file:   src/main/resources/static/fr.jpg 	new file:   src/main/resources/static/gd.jpg 	new file:   src/main/resources/static/rus.jpg 	new file:   src/main/resources/static/sample.pdf

--- src/main/java/com/pankaj/springrestexample/HelloWordController.java
+++ src/main/java/com/pankaj/springrestexample/HelloWordController.java
@@ -1,11 +1,26 @@
 package com.pankaj.springrestexample;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
 import java.util.Locale;
 
+import javax.activation.FileTypeMap;
+
+import org.apache.commons.io.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.StreamUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -14,8 +29,33 @@ public class HelloWordController {
 	MessageSource messageSource;
 	
 	@GetMapping("/helloWorld")
-	public String Greeting(@RequestHeader(name="Accept-Language",required = false,defaultValue = "US") Locale locale) {
-		return messageSource.getMessage("good.morning.message",null, locale);
+	//public String Greeting(@RequestHeader(name="Accept-Language",required = false,defaultValue = "US") Locale locale) {
+	// by default it will pick up locale.us
+	public String Greeting() {
+		return messageSource.getMessage("good.morning.message",null, LocaleContextHolder.getLocale());
 	}
+	
+// serving image in broswer
+	 @RequestMapping(value = "/get-images", method = RequestMethod.GET,
+	            produces = MediaType.IMAGE_JPEG_VALUE)
+	    public ResponseEntity<byte[]> getImage() throws IOException {
+	    	String message = messageSource.getMessage("image-path",null, LocaleContextHolder.getLocale());
+	        ClassPathResource imgFile = new ClassPathResource(message);
+	        byte[] bytes = StreamUtils.copyToByteArray(imgFile.getInputStream());
 
+	        return ResponseEntity
+	                .ok()
+	                .contentType(MediaType.IMAGE_JPEG)
+	                .body(bytes);
+	    }
+
+	// serving pdf in broswer
+	@GetMapping("thing")
+	public ResponseEntity<byte[]> what() throws IOException{
+	    File file = new File("src/main/resources/static/sample.pdf");
+	    return ResponseEntity.ok()
+	            .header("Content-Disposition", "attachment; filename=" +file.getName())
+	            .contentType(MediaType.valueOf(FileTypeMap.getDefaultFileTypeMap().getContentType(file)))
+	            .body(Files.readAllBytes(file.toPath()));
+	}
 }
